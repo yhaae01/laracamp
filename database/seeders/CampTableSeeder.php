0b2c795b42e4a7eebda931bb2cd3e06cab829842
yhaae01 <suryaintpermana@gmail.com>
2022-04-07 05:39:23 +0700

    Seed Camp Table

---
+++ database/seeders/CampTableSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Camp;
+use Illuminate\Database\Seeder;
+
+class CampTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $camps = [
+            [
+                'title' => 'Gila Belajar',
+                'slug' => 'gila-belajar',
+                'price' => 280,
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ],
+            [
+                'title' => 'Baru Mulai',
+                'slug' => 'baru-mulai',
+                'price' => 140,
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ],
+        ];
+
+        // 1st method
+        // Tidak perlu menggunakan created_at dan updated_at
+        // foreach ($camps as $key => $camp) {
+        //     Camp::create($camp);
+        // }
+
+        // 2nd method
+        // Harus menggunakan created_at dan updated_at
+        Camp::insert($camps);
+    }
+}
