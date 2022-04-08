4080aba79ae6273e77501866a0462af46cdeef10
yhaae01 <suryaintpermana@gmail.com>
2022-04-09 06:16:23 +0700

    Integration Landing Page, Checkout and My Dashboard

--- resources/views/checkout/create.blade.php
+++ resources/views/checkout/create.blade.php
@@ -23,8 +23,8 @@
                         <div class="col-lg-5 col-12">
                             <div class="item-bootcamp">
                                 <img src="{{ asset('/images/item_bootcamp.png') }}" alt="" class="cover">
-                                <h1 class="package">
-                                    GILA BELAJAR
+                                <h1 class="package text-uppercase">
+                                    {{ $camp->title }}
                                 </h1>
                                 <p class="description">
                                     Bootcamp ini akan mengajak Anda untuk belajar penuh mulai dari pengenalan dasar sampai membangun sebuah projek asli
@@ -33,32 +33,33 @@
                         </div>
                         <div class="col-lg-1 col-12"></div>
                         <div class="col-lg-6 col-12">
-                            <form action="{{ route('success-checkout') }}" class="basic-form">
+                            <form action="{{ route('checkout.store', $camp->id) }}" method="POST" class="basic-form">
+                                @csrf
                                 <div class="mb-4">
-                                    <label for="exampleInputEmail1" class="form-label">Full Name</label>
-                                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                    <label class="form-label">Full Name</label>
+                                    <input type="text" name="name" class="form-control" value="{{ Auth::user()->name }}">
                                 </div>
                                 <div class="mb-4">
-                                    <label for="exampleInputEmail1" class="form-label">Email Address</label>
-                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                    <label class="form-label">Email Address</label>
+                                    <input type="email" name="email" class="form-control" value="{{ Auth::user()->email }}">
                                 </div>
                                 <div class="mb-4">
-                                    <label for="exampleInputEmail1" class="form-label">Occupation</label>
-                                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                    <label class="form-label">Occupation</label>
+                                    <input type="text" name="occupation" class="form-control" value="{{ Auth::user()->occupation }}">
                                 </div>
                                 <div class="mb-4">
-                                    <label for="exampleInputEmail1" class="form-label">Card Number</label>
-                                    <input type="number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                    <label class="form-label">Card Number</label>
+                                    <input type="number" name="card_number" class="form-control" value="{{ Auth::user()->card_number }}">
                                 </div>
                                 <div class="mb-5">
                                     <div class="row">
                                         <div class="col-lg-6 col-12">
-                                            <label for="exampleInputEmail1" class="form-label">Expired</label>
-                                            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                            <label class="form-label">Expired</label>
+                                            <input type="month" name="expired" class="form-control">
                                         </div>
                                         <div class="col-lg-6 col-12">
-                                            <label for="exampleInputEmail1" class="form-label">CVC</label>
-                                            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                            <label class="form-label">CVC</label>
+                                            <input type="number" maxlength="3" name="cvc" class="form-control" value="{{ Auth::user()->cvc }}">
                                         </div>
                                     </div>
                                 </div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\User\CheckoutController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,20 +20,17 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-Route::get('checkout', function () {
-    return view('checkout');
-})->name('checkout');
-
-Route::get('success-checkout', function () {
-    return view('success_checkout');
-})->name('success-checkout');
-
 // Socialite routes
 Route::get('sign-in-google', [UserController::class, 'google'])->name('user.login.google');
 Route::get('auth/google/callback', [UserController::class, 'handleProviderCallback'])->name('user.google.callback');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('checkout/success', [CheckoutController::class, 'success'])->name('checkout.success');
+    Route::get('checkout/{camp:slug}', [CheckoutController::class, 'create'])->name('checkout.create');
+    Route::post('checkout/{camp}', [CheckoutController::class, 'store'])->name('checkout.store');
+
+    Route::get('dashboard', [HomeController::class, 'dashboard'])->name('dashboard');
+});
 
 require __DIR__.'/auth.php';
